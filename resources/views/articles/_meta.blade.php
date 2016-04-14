41d55c672b6f4314bb393a2287edb7a04b6a6487
Specs <464089419@qq.com>
2016-04-14 09:25:11 +0800

    fix author permission

--- resources/views/articles/_meta.blade.php
+++ resources/views/articles/_meta.blade.php
@@ -2,6 +2,6 @@
 <i class="fa fa-user"></i> <a href="/user/{{ $article->user->id }}">{{ $article->user->name }}</a>
 <i class="fa fa-eye"></i> {{ $article->view_count }} views
 <i class="fa fa-calendar"></i> {{ $article->created_at->diffForHumans() }}
-@can('update', $article)
+@can('owns', $article)
     <i class="fa fa-edit"></i> <a href="/article/{{ $article->id }}/edit">Update</a>
 @endcan
