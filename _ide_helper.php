005b51c09438394fbf27faa131fa06a0884696f4
vagrant <vagrant@homestead>
2016-05-10 10:02:38 +0000

    ignore ide-helper

--- _ide_helper.php
+++ _ide_helper.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.1.27 (LTS) on 2016-05-04.
+ * Generated for Laravel 5.1.27 (LTS) on 2016-05-10.
  *
  * @author Barry vd. Heuvel <barryvdh@gmail.com>
  * @see https://github.com/barryvdh/laravel-ide-helper
